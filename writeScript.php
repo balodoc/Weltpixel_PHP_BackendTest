b0738bbb87e3a67d79df3773b03655dbb0f3eddb
rtacadena <tacadena.roland@gmail.com>
2024-02-22 14:50:22 +0800

    working sample

--- writeScript.php
+++ writeScript.php
@@ -10,6 +10,10 @@ use Services\Presenter\OrderItems;
 
 require 'vendor/autoload.php';
 
+/**
+ * 1. Get all orders from the database that are from Weltpixel
+ * Hint: Utilize the Database implementation to connect and retrieve data;
+ */
 $orderService = new OrderDbService(new MysqlDbHandler());
 $options = [
     'conditions' => [
@@ -17,14 +21,31 @@ $options = [
         'OrderItems_Item_ItemStatus' => ['operator' => '=', 'value' => OrderItems::ITEM_STATUS_SHIPPED]
     ]
 ];
+
 $shippedOrders = $orderService->fetchAll($options);
 
+/**
+ * 2. Send an email notification to the OrderEmail field for the items with status Item Shipped
+ * that includes a total amount and the order details(name, date, item id, item price)
+ * Hints:
+ *      A) Make use of Presenter model classes to put the order&item data into objects;
+ *      B) Write a presenter class(that implements PresenterInterface) to format the order;
+ *      C) Create a email notifier implementation to use along with PHPmailer package;
+ */
+
+/**
+ * 2. A)
+ * 2. B)
+ */
 $presenter = new OrderDataPresenter();
 $shippedOrderObjects = $presenter->format($shippedOrders);
 
 $emailNotifier = new EmailNotifier();
 $emailFormatter = new OrderEmailFormatter();
 
+/**
+ * 2. C)
+ */
 if ($emailNotifier->connect()) {
     foreach ($shippedOrderObjects as $orderObject) {
         $name = $orderObject->getFirstName() . ' ' . $orderObject->getLastName();
@@ -39,6 +60,10 @@ if ($emailNotifier->connect()) {
     }
 }
 
+/**
+ * 3. Update the order records that you sent notification for and set the item
+ * statuses to ItemStatus = 'Notification sent' & ItemStatusId = 10;
+ */
 $itemIdsToUpdate = [];
 /** @var OrderData $orderData */
 foreach ($shippedOrderObjects as $orderData) {
